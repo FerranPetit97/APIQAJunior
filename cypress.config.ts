4737162c52da509acc45fdf50cf1f24219d175d4
Ferran Petit <fpmartiwork@gmail.com>
2022-11-17 22:19:09 +0100

    Cypress integration

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: '2wa6o4',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
